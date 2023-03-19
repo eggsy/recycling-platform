8472364685cefe1bd7e3de041886141ecb1c9b36
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-19 17:44:34 +0300

    feat: create people card component

---
+++ src/components/PeopleCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export const PeopleCard = ({
+  image,
+  name,
+  linkedIn,
+}: {
+  image: string;
+  name: string;
+  linkedIn: string;
+}) => {
+  return (
+    <a
+      href={`https://linkedin.com/in/${linkedIn}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Open LinkedIn profile"
+      className="inline-flex h-10 items-center rounded-lg bg-black/10 px-3 py-1 align-middle text-sm font-medium no-underline transition-colors hover:bg-black/20"
+    >
+      <Image
+        src={image}
+        alt="Person image"
+        width={30}
+        height={30}
+        className="mr-2 rounded-full"
+      />
+      {name}
+    </a>
+  );
+};
