1d14e505ff5cd2f00945ff6954a29dfee6fffd05
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-20 00:47:14 +0300

    refactor: handle search and deletion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,18 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Image from "next/image";
 import { useAtom } from "jotai";
 import { motion, AnimatePresence } from "framer-motion";
+import { deleteDoc, doc } from "firebase/firestore";
+import { toast } from "sonner";
+import { firestore, storage } from "@/firebase/clientApp";
+
+// Store
 import { categoriesAtom } from "@/store/categories";
 import { itemsAtom } from "@/store/items";
+import { authAtom } from "@/store/auth";
 
 // Components
 import { Card } from "@/components/Card";
+import { deleteObject, ref } from "firebase/storage";
 
 export default function Home() {
   const [search, setSearch] = useState("");
@@ -22,28 +29,48 @@ export default function Home() {
 
   const [categories, setCategories] = useAtom(categoriesAtom);
   const [items, setItems] = useAtom(itemsAtom);
+  const [authCache] = useAtom(authAtom);
 
-  const getFilteredResults = useMemo(() => {
-    if (categories.selectedCategoryId) {
-      return items.items.filter((i) =>
-        i.name.toLowerCase().includes(search.toLowerCase())
-      );
-    }
+  const getFilteredItems = useMemo(() => {
+    return items.items.filter((i) =>
+      i.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search, items.items]);
 
+  const getFilteredCategories = useMemo(() => {
     return categories.categories.filter((c) =>
       c.name.toLowerCase().includes(search.toLowerCase())
     );
-  }, [
-    search,
-    items.items,
-    categories.selectedCategoryId,
-    categories.categories,
-  ]);
+  }, [search, categories.categories]);
 
   const getSelectedItem = useMemo(() => {
     if (!items.selectedItemId) return null;
     return items.items.find((c) => c.id === items.selectedItemId);
   }, [items.items, items.selectedItemId]);
+
+  const handleItemDelete = async (itemId: string) => {
+    const sure = confirm("Are you sure you want to delete this item?");
+
+    if (!sure) {
+      toast.error("Item deletion cancelled.");
+      return;
+    }
+
+    await deleteObject(ref(storage, getSelectedItem?.image)).catch((err) => {
+      toast.error(err.message);
+    });
+
+    await deleteDoc(doc(firestore, `items/${itemId}`)).catch((err) => {
+      toast.error(err.message);
+    });
+
+    setItems((p) => ({
+      ...p,
+      items: p.items.filter((i) => i.id !== itemId),
+    }));
+
+    toast.success("Item deleted successfully.");
+  };
 
   return (
     <div className="flex flex-col gap-8 md:h-[80vh] md:max-h-[1000px] md:flex-row">
@@ -78,20 +105,37 @@ export default function Home() {
             </li>
           )}
 
-          {getFilteredResults.length === 0 && (
-            <li className="my-4 block text-center">No results found...</li>
+          {categories.selectedCategoryId && getFilteredItems.length === 0 && (
+            <li className="my-4 block text-center">No items found...</li>
           )}
 
-          {getFilteredResults.map((item, index) => (
-            <li key={item.name}>
-              <Card
-                id={item.id}
-                type={categories.selectedCategoryId ? "item" : "category"}
-                name={item.name}
-                image={`https://picsum.photos/100/100?random=${index}`}
-              />
-            </li>
-          ))}
+          {!categories.selectedCategoryId &&
+            getFilteredCategories.length === 0 && (
+              <li className="my-4 block text-center">No categories found...</li>
+            )}
+
+          {categories.selectedCategoryId
+            ? getFilteredItems.map((item) => (
+                <li key={item.id}>
+                  <Card
+                    id={item.id}
+                    image={item.image}
+                    type={categories.selectedCategoryId ? "item" : "category"}
+                    decomposeTime={item.decomposeTime}
+                    name={item.name}
+                  />
+                </li>
+              ))
+            : getFilteredCategories.map((category) => (
+                <li key={category.id}>
+                  <Card
+                    id={category.id}
+                    image={category.image}
+                    type="category"
+                    name={category.name}
+                  />
+                </li>
+              ))}
         </ul>
       </aside>
 
@@ -120,35 +164,52 @@ export default function Home() {
             }}
             className="flex flex-grow flex-col space-y-4 rounded-lg bg-gray-100/70 ring-1 ring-black/10 backdrop-blur-md"
           >
-            <header className="flex items-center gap-3 rounded-t-lg bg-white px-6 py-3">
-              <TbX
-                size={24}
-                className="cursor-pointer rounded-full bg-red-600/10 p-1 text-red-600 transition-colors hover:bg-red-600 hover:text-white"
-                onClick={() =>
-                  setItems((p) => ({ ...p, selectedItemId: null }))
-                }
-              />
+            <header className="flex justify-between gap-6 rounded-t-lg bg-white px-6 py-3">
+              <div className="flex items-center gap-3">
+                <TbX
+                  size={24}
+                  className="cursor-pointer rounded-full bg-red-600/10 p-1 text-red-600 transition-colors hover:bg-red-600 hover:text-white"
+                  onClick={() =>
+                    setItems((p) => ({ ...p, selectedItemId: null }))
+                  }
+                />
 
-              <h1 className="font-medium">{getSelectedItem.name}</h1>
+                <h1 className="font-medium">{getSelectedItem.name}</h1>
+              </div>
+
+              {authCache.isAdmin && (
+                <div className="flex items-center space-x-2">
+                  <button
+                    type="button"
+                    className="flex items-center justify-center rounded-lg bg-red-600/20 px-4 py-1 text-sm text-red-600 transition-colors hover:bg-red-600/40"
+                    onClick={() => handleItemDelete(getSelectedItem.id)}
+                  >
+                    <TbX />
+                    <span>Delete</span>
+                  </button>
+                </div>
+              )}
             </header>
 
             <div className="flex flex-col gap-4 px-6 py-4">
               <section className="flex flex-col-reverse justify-between gap-4 md:flex-row">
                 <div className="flex flex-col space-y-8">
                   <p className="text-black/80">
-                    <span className="rounded-lg bg-black/10 px-3 py-1.5 align-middle font-medium">
+                    <span className="rounded-lg bg-black/10 px-3 py-1.5 align-middle text-sm font-medium">
                       {getSelectedItem.name}
                     </span>{" "}
                     take(s){" "}
-                    <span className="inline-flex items-center rounded-lg bg-red-600/10 px-3 py-1 align-middle font-medium text-red-600">
+                    <span className="inline-flex items-center rounded-lg bg-red-600/10 px-3 py-1 align-middle text-sm font-medium text-red-600">
                       <TbClock className="mr-1.5" />
                       {getSelectedItem.decomposeTime}
                     </span>{" "}
                     to decompose. We can protect our environment by throwing
                     them into{" "}
-                    <span className="inline-flex items-center rounded-lg bg-green-600/10 px-3 py-1 align-middle font-medium text-green-600">
+                    <span className="inline-flex items-center rounded-lg bg-green-600/10 px-3 py-1 align-middle text-sm font-medium text-green-600">
                       <TbRecycle className="mr-1.5" />
-                      {getSelectedItem.wasteType}
+                      {categories.categories
+                        .find((i) => i.id === getSelectedItem.categoryId)
+                        ?.name.toLowerCase()}
                     </span>{" "}
                     bins.
                   </p>
