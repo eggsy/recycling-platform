c408170ebfa7919b85ffde0139260ce9c7f70e86
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-19 16:50:56 +0300

    chore: more pwa configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,64 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const title = "Recycling App";
+  const description =
+    "Wonder what happens if you don't recyle? Take a look at items on our database to see what impacts could be done if you recyle.";
+  const themeColor = "#145A32";
+  const url = "https://recylcing-app.netlify.app";
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content={title} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={title} />
+        <meta name="description" content={description} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content={themeColor} />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content={themeColor} />
+
+        <link rel="apple-touch-icon" href="/icons/512x512.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/icons/192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="384x384"
+          href="/icons/384x384.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="/icons/512x512.png"
+        />
+
+        <link rel="icon" type="image/png" href="/icons/192x192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="mask-icon"
+          href="/icons/icons/384x384.png"
+          color={themeColor}
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={`${url}/icons/512x512.png`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:url" content={url} />
+        <meta property="og:image" content={`${url}/icons/512x512.png`} />
+      </Head>
 
       <body className="bg-[url('/background.png')] bg-fixed bg-center bg-cover">
         <Main />
