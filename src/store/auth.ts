824d83401bdef3322098ce0365e48231f50151eb
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-20 09:21:33 +0300

    chore: add merlos uid

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -6,7 +6,10 @@ interface IAuth {
   isAdmin?: boolean;
 }
 
-export const adminUids = ["ONv2wcb0FcS82mpZiYE1EZDgmHs1"];
+export const adminUids = [
+  "ONv2wcb0FcS82mpZiYE1EZDgmHs1",
+  "pBJDyTBUlRW73h9XHaV3E2A9HB82",
+];
 
 export const authAtom = atom<IAuth>({
   user: null,
