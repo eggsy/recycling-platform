4d3a0b9aabf90e2b8911232acd78e67a7d75bddb
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-20 20:33:01 +0300

    feat: get user values from database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { toast } from "sonner";
+import { IUser } from "@/store/auth";
 
 initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -25,7 +32,16 @@ isSupported().then((supported) => {
   if (supported) analytics = getAnalytics();
 });
 
-export const signInPopup = () =>
+export const signInPopup = () => {
+  const sure = confirm(
+    "By logging you agree to share your display name and avatar with us."
+  );
+
+  if (!sure) {
+    toast.error("Okay.");
+    return;
+  }
+
   signInWithPopup(auth, googleProvider)
     .then((user) => {
       toast.success(`Welcome ${user.user.displayName}!`);
@@ -38,5 +54,65 @@ export const signInPopup = () =>
 
       console.error(errorCode, errorMessage, email, credential);
     });
+};
+
+export const getCurrentUser = async () => {
+  const user = auth.currentUser;
+
+  if (!user?.uid) return null;
+
+  return await getDoc(doc(firestore, `users/${user.uid}`))
+    .then(async (userDoc) => {
+      if (!userDoc.exists()) {
+        const newUser = await createUser({
+          displayName: user.displayName,
+          avatar: user.photoURL,
+          lastUpdatedAt: new Date(),
+          uid: user.uid,
+        });
+
+        return newUser as IUser;
+      }
+
+      return userDoc.data() as IUser;
+    })
+    .catch((err) => {
+      toast.error(err.message);
+      return null;
+    });
+};
+
+const createUser = async ({
+  displayName,
+  avatar,
+  lastUpdatedAt,
+  uid,
+}: {
+  displayName: string | null;
+  avatar: string | null;
+  lastUpdatedAt: Date;
+  uid: string;
+}) => {
+  const dataObject = {
+    displayName,
+    avatar,
+    lastUpdatedAt,
+    score: 0,
+  };
+
+  await setDoc(doc(firestore, `users/${uid}`), dataObject);
+  return dataObject;
+};
+
+export const updateScore = async (score: number) => {
+  const user = auth.currentUser;
+
+  if (user) {
+    await updateDoc(doc(firestore, `users/${user.uid}`), {
+      lastUpdatedAt: new Date(),
+      score,
+    });
+  }
+};
 
 export { firestore, auth, storage, analytics };
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,17 +1,21 @@
 import { atom } from "jotai";
 import type { User } from "firebase/auth";
 
-interface IAuth {
-  user: User | null;
+export interface IUser {
+  displayName: string;
+  avatar: string;
+  score: number;
   isAdmin?: boolean;
 }
 
-export const adminUids = [
-  "ONv2wcb0FcS82mpZiYE1EZDgmHs1",
-  "pBJDyTBUlRW73h9XHaV3E2A9HB82",
-];
+interface IAuth {
+  user: User | null;
+  userDb: IUser | null;
+  isAdmin?: boolean;
+}
 
 export const authAtom = atom<IAuth>({
   user: null,
+  userDb: null,
   isAdmin: false,
 });
