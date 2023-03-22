dad430fa7ff1b7cd5e09b8e3c5c99ec90dd2d89f
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-22 12:29:29 +0300

    fix: do re-render only on content pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -55,7 +55,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const [isActive, setActive] = useState(false);
 
   const debouncedScore = useDebounce(score, 1500);
-  const { asPath } = useRouter();
+  const { asPath, pathname } = useRouter();
 
   const mouseSensor = useSensor(MouseSensor);
   const touchSensor = useSensor(TouchSensor);
@@ -193,7 +193,7 @@ export default function App({ Component, pageProps }: AppProps) {
       >
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
-            key={asPath}
+            key={pathname === "/[slug]" ? asPath : pathname}
             initial={{
               opacity: 0,
               scale: 0.9,
